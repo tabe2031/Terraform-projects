b85697baa7be6f0d43816cc8f0342bc9a367a265
tabe2031 <doctorwiki6@gmail.com>
2023-12-17 21:51:46 +0100

    updated

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Helm-and-Helmfile
+
+#To Debug terraform, we use LOG and set an environmental variable as {TF_lOG=TRACE}. this will enable us to see detailly the processes runned by terraform.
+         and
+         if we intend to export the logs we are gathering to help us debug out of the environment of our terraform resource, we run {TF_LOG_PATH= path to where we intend to store the logs for later reviewing]
--- challenge-2/main.tf
+++ challenge-2/main.tf
@@ -1,70 +1,70 @@
 provider "aws" {
-    region = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "db" {
-    ami = "ami-032598fcc7e9d1c7a"
-    instance_type = "t2.micro"
+  ami           = "ami-032598fcc7e9d1c7a"
+  instance_type = "t2.micro"
 
-    tags = {
-        Name = "DB Server"
-    }
+  tags = {
+    Name = "DB Server"
+  }
 }
 
 resource "aws_instance" "web" {
-    ami = "ami-032598fcc7e9d1c7a"
-    instance_type = "t2.micro"
-    security_groups = [aws_security_group.web_traffic.name]
-    user_data = file("script.sh")
-    tags = {
-        Name = "Web Server"
-    }
+  ami             = "ami-032598fcc7e9d1c7a"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.web_traffic.name]
+  user_data       = file("script.sh")
+  tags = {
+    Name = "Web Server"
+  }
 }
 
 resource "aws_eip" "web_ip" {
-    instance = aws_instance.web.id
+  instance = aws_instance.web.id
 }
 
 variable "ingress" {
-    type = list(number)
-    default = [80,443]
+  type    = list(number)
+  default = [80, 443]
 }
 
 variable "egress" {
-    type = list(number)
-    default = [80,443]
+  type    = list(number)
+  default = [80, 443]
 }
 
 resource "aws_security_group" "web_traffic" {
-    name = "Allow Web Traffic"
+  name = "Allow Web Traffic"
 
-    dynamic "ingress" {
-        iterator = port
-        for_each = var.ingress
-        content {
-            from_port = port.value
-            to_port = port. value
-            protocol = "TCP"
-            cidr_blocks = ["0.0.0.0/0"]
-        }
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingress
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
     }
+  }
 
-        dynamic "egress" {
-        iterator = port
-        for_each = var.egress
-        content {
-            from_port = port.value
-            to_port = port. value
-            protocol = "TCP"
-            cidr_blocks = ["0.0.0.0/0"]
-        }
+  dynamic "egress" {
+    iterator = port
+    for_each = var.egress
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
     }
+  }
 }
 
 output "PrivateIP" {
-    value = aws_instance.db.private_ip
+  value = aws_instance.db.private_ip
 }
 
 output "PublicIP" {
-    value = aws_eip.web_ip.public_ip
+  value = aws_eip.web_ip.public_ip
 }
